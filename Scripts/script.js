a8e542fa444fb3a5af91509d40bae2b46425da32
kenny herillard <Herillard>
2020-05-04 11:15:01 +0200

    Ajout de la recherche avec le champ de recherche et l'options de tri

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,18 @@
-$(function() {
-    
+$(function () {
+    $(".dropdown-menu a").click(function () {
+        let dropdown = $("#dropdown-options");
+        dropdown.html($(this).html());
+        dropdown.attr("value", $(this).attr("value"));
+    })
+
+    $("#search-submit").click(function (e) {
+        e.preventDefault();
+        let url = `https://api.deezer.com/search?q=${$("#search").val()}&order=${$("#dropdown-options").attr("value")}&output=jsonp`;
+        $.ajax({
+            url: url,
+            dataType: "jsonp",
+        }).then((result) => {
+            console.log("Résultat :", result.data);
+        });
+    })
 })
