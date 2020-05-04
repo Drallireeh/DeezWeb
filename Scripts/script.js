80b25eaf86202dbcb17158dcbc6e19e2e28f8b70
kenny herillard <Herillard>
2020-05-04 14:10:46 +0200

    Ajout et suppression de favoris fonctionnels

--- Scripts/script.js
+++ Scripts/script.js
@@ -22,12 +22,14 @@ function OnSearch() {
             $("#no-result").show();
         } else {
             for (let i = 0; i < result.data.length; i++) {
-                console.log(result.data[i])
                 $("#search-result").append(`
                     <div class="track">
-                        <div>
-                            <p class="title ellipsis">${result.data[i].title}</p>
-                            <p class="artist-album ellipsis">${result.data[i].artist.name} / ${result.data[i].album.title}</p>
+                        <div class="infos-track">
+                            <img src=${result.data[i].album.cover_small} alt="cover album">
+                            <div>
+                                <p class="title ellipsis">${result.data[i].title}</p>
+                                <p class="artist-album ellipsis">${result.data[i].artist.name} / ${result.data[i].album.title}</p>
+                            </div>
                         </div>
                         <audio
                             controls
@@ -35,10 +37,65 @@ function OnSearch() {
                             Your browser does not support the
                             <code>audio</code> element.
                         </audio>
-                        <button class="add-favorites">Ajouter aux favoris</button>
+                        <button class="btn btn-secondary add-favorites">Ajouter aux favoris</button>
                     </div>
-                `)
+                `);
+
+                let favorites = JSON.parse(sessionStorage.getItem("favoris"));
+                if (favorites) {
+                    for (let j = 0; j < favorites.length; j++) {
+                        if (favorites[j].id === result.data[i]) {
+                            $(".add-favorites:last").addClass("favorite-added");
+                            $(".add-favorites:last").html("Retirer un favoris");
+                        }
+                    }
+                }
+
+                $(".add-favorites:last").click(function () {
+                    let el = $(this);
+                    if (el.hasClass("favorite-added")) {
+                        RemoveFavorites(result.data[i]);
+                        el.removeClass("favorite-added");
+                        el.html("Ajouter un favoris");
+                    }
+                    else {
+                        AddFavorites(result.data[i]);
+                        el.addClass("favorite-added");
+                        el.html("Retirer un favoris");
+                    }
+                });
             }
         }
     });
+}
+
+function AddFavorites(data) {
+    let listFavorites = [];
+    let favorites = sessionStorage.getItem("favoris");
+    if (favorites) {
+        let parsedFavorites = JSON.parse(favorites);
+        for (let i = 0; i < parsedFavorites.length; i++) {
+            if (data.id === parsedFavorites[i].id) continue;
+            listFavorites.push(parsedFavorites[i]);
+        }
+    }
+    listFavorites.push(data)
+    let json = JSON.stringify(listFavorites);
+    sessionStorage.setItem("favoris", json);
+}
+
+function RemoveFavorites(data) {
+    let favorites = sessionStorage.getItem("favoris");
+    let parsedFavorites = JSON.parse(favorites);
+    for (let i = 0; i < parsedFavorites.length; i++) {
+        if (data.id === parsedFavorites[i].id) {
+            parsedFavorites.splice(i, 1);
+        }
+    }
+    let json = JSON.stringify(parsedFavorites);
+    sessionStorage.setItem("favoris", json);
+}
+
+function DisplayFavorites() {
+
 }
